9dd42f636913a4cc54742887866697397836a10c
damachilami <lami.damachi@heig-vd.ch>
2025-09-16 12:32:33 +0000

    added TD20250916

---
+++ TD20250916/td.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	unsigned int num_students = 23;
+	// signed int m = -42; // signed is optional
+	int account_value = -42;
+	float temperature = 18.1;	   // 32 bits
+	double pi = 3.141592653589793; // 64 bits
+}
